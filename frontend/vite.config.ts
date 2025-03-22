92d10e3822bc3fbb339bd3a983d71a6970389dca
Erik Torvbraaten <erik.torvbraten@outlook.com>
2025-03-22 19:08:22 +0100

    url configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ai-kidney-diagnosis-webapp-fron2/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
